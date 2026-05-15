bced8c8d17669fd0e08a51d0307c7fefbaf31efb
Arjun Raj <arjunrajlab@gmail.com>
2026-05-15 09:52:09 -0400

    Fix collection chips in location chooser

--- src/components/GirderLocationChooser.test.ts
+++ src/components/GirderLocationChooser.test.ts
@@ -15,8 +15,30 @@ function mountComponent(props = {}) {
     },
     global: {
       stubs: {
-        CustomFileManager: true,
+        CustomFileManager: {
+          name: "CustomFileManager",
+          props: [
+            "location",
+            "itemsPerPage",
+            "itemsPerPageOptions",
+            "menuEnabled",
+            "moreChips",
+            "clickableChips",
+          ],
+          template: "<div />",
+        },
         GirderBreadcrumb: true,
+        VDialog: {
+          props: ["modelValue"],
+          template:
+            "<div><slot name='activator' :props='{}' /><slot /></div>",
+        },
+        VCard: { template: "<div><slot /></div>" },
+        VCardTitle: { template: "<div><slot /></div>" },
+        VCardText: { template: "<div><slot /></div>" },
+        VCardActions: { template: "<div><slot /></div>" },
+        VSpacer: { template: "<div />" },
+        VBtn: { template: "<button><slot /></button>" },
       },
     },
   });
@@ -74,5 +96,13 @@ describe("GirderLocationChooser", () => {
     const newVal = { name: "New Folder", _modelType: "folder" };
     await wrapper.setProps({ modelValue: newVal as any });
     expect(wrapper.vm.selected).toEqual(newVal);
+  });
+
+  it("keeps collection relation chips enabled but not clickable", () => {
+    const wrapper = mountComponent();
+    const fileManager = wrapper.findComponent({ name: "CustomFileManager" });
+
+    expect(fileManager.props("moreChips")).toBeUndefined();
+    expect(fileManager.props("clickableChips")).toBe(false);
   });
 });
